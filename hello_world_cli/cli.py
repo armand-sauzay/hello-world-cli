dc80ff35c337554a9d79dbb6081423ae2ef1a5a0
Armand Sauzay <35524799+armand-sauzay@users.noreply.github.com>
2023-05-16 02:59:57 +0000

    feat: initial repository setup

---
+++ hello_world_cli/cli.py
@@ -0,0 +1,28 @@
+import argparse
+
+import requests
+
+
+def main(argv=None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--name", help="The name to greet and count repos for", default="world"
+    )
+    args = parser.parse_args(argv)
+    if args.name == "":
+        print("Username cannot be empty")
+        return 1
+
+    print(f"Hello {args.name}!")
+
+    repos = requests.get(f"https://api.github.com/users/{args.name}/repos")
+    if repos.status_code != 200:
+        print(f"Failed to fetch repos for {args.name}")
+        return 1
+    repos = repos.json()
+    print(f"You have {len(repos)} repos.")  # type: ignore
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())

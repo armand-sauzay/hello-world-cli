f98a1ba6c84d2786883de9f43550992c2c15024a
Armand Sauzay <35524799+armand-sauzay@users.noreply.github.com>
2023-05-16 02:59:57 +0000

    feat: initial repository setup

--- README.md
+++
@@ -1,8 +0,0 @@
-# python-template
-
-Template for python-based repositories.
-
-## Usage
-
-1. Use this repository by specifying it as a template for your repository.
-2. run `./script/bootstrap`
---
+++ tests/cli_test.py
@@ -0,0 +1,17 @@
+from hello_world_cli.cli import main
+
+
+def test_main(capsys):
+    assert main(["--name", "test"]) == 0
+    out, err = capsys.readouterr()
+    # test is a user which does not have any contributions since 2010.
+    # Hopefully this will not change in the future and the test will not break.
+    assert out == "Hello test!\nYou have 5 repos.\n"
+    assert err == ""
+
+
+def test_main_empty_name(capsys):
+    assert main(["--name", ""]) == 1
+    out, err = capsys.readouterr()
+    assert out == "Username cannot be empty\n"
+    assert err == ""
